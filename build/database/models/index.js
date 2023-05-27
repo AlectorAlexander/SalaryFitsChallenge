6f27559d937c91327f35a15595061fca9ebd01bf
Aléctor Alexander <alector.alexandre@gmail.com>
2023-05-27 20:00:37 -0300

    feat: Atualizações no arquivo package.json
    Adiciona as dependências necessárias e atualizações de scripts no arquivo package.json. Isso inclui a adição do script install-start para facilitar a instalação e a inicialização do projeto, bem como a inclusão de novas dependências relacionadas ao banco de dados e aos testes.

--- build/database/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const sequelize_1 = require("sequelize");
-const config = require("../config/sequelize.config");
-const sequelize = new sequelize_1.Sequelize(config[process.env.NODE_ENV || 'development']);
-exports.default = sequelize;
-//# sourceMappingURL=index.js.map
